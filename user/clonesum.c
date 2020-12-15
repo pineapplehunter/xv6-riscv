3da84f2a68af347293cafe4dc4dc0876ac3109ef
Shogo Takata <pineapplehunter.daniel@gmail.com>
2020-12-15 10:09:00 +0900

    update clonesum.c
    Max process count was 64, so this program was not being able to calculate the right value.
    Fixid by using the return value of clone.

--- user/clonesum.c
+++ user/clonesum.c
@@ -7,21 +7,27 @@ long sum = 0;
 int
 runner(void *args)
 {
-  for(uint64 i = 0; i < 100; i++)
-    sum++;
+  for(uint64 i = 0; i < 100; i++) {
+    long s = sum;
+    for(int k = 0; k < 1000; k++)
+      ;
+    sum = s + 1;
+  }
+
   exit(0);
 }
 
 int
 main(int argc, char *argv[])
 {
-
-  for(int i = 0; i < 100; i++)
-    clone(runner, (void *)0);
-  for(int i = 0; i < 100; i++)
+  int procs_count;
+  for(procs_count = 0; procs_count < 100; procs_count++)
+    if(clone(runner, (void *)0) < 0)
+      break;
+  for(int i = 0; i < procs_count; i++)
     wait(0);
 
-  printf("sum should be %d\n", 100 * 100);
+  printf("sum should be %d\n", 100 * procs_count);
   printf("thread sum is %d\n", sum);
   exit(0);
 }
